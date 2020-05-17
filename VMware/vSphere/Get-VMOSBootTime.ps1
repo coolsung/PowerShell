bb23db6ecdee03c9bf9c343a221863e27b606401
coolsung <coolsung@gmail.com>
2020-05-17 19:08:15 +0900

    master

---
+++ VMware/vSphere/Get-VMOSBootTime.ps1
@@ -0,0 +1,13 @@
+﻿## DarkBlack.Dev
+## Last OS Boot Time for VM
+
+$Stat = 'sys.osuptime.latest'
+
+$Now = Get-Date
+$VMs = Get-VM
+
+Get-Stat -Entity $VMs -Stat $stat -Realtime -MaxSamples 1 |
+
+Select-Object @{N='VM';E={$_.Entity.Name}},
+    @{N='LastOSBoot';E={$now.AddSeconds(- $_.Value)}},
+    @{N='UptimeDays';E={[math]::Floor($_.Value/(24*60*60))}} # 24 Hours * 60 Min * 60 Sec
