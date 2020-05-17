226a719e5bdf200af119ab36842ec7107c57d6e9
coolsung <coolsung@gmail.com>
2020-05-17 18:00:44 +0900

    master

---
+++ VMware/vSphere/Set-ESXiHostNTP.ps1
@@ -0,0 +1,34 @@
+﻿# vCenter Server에 등록된 모든 ESXi Host에 대해서 NTP 정보를 삭제 하고 새로 등록 합니다.
+
+
+
+
+$NTPServers = "time.bora.net", "172.16.12.41"
+
+
+$esxHosts = Get-VMHost
+
+#No need to edit anything below this line
+#====================================
+foreach ($esx in $esxHosts){
+    $AllNTP = Get-VMHostNtpServer -VMHost $esx
+    foreach ($ThisNTP in $AllNTP){
+        Write-Host "Removing $ThisNTP from $esx" -ForegroundColor Yellow
+        Remove-VMHostNtpServer -VMHost $esx -ntpserver $ThisNTP -Confirm:$false
+
+    }
+    foreach ($ThisNTP in $NTPServers){
+        Write-Host "Adding $ThisNTP to $esx" -ForegroundColor Green
+        add-vmhostntpserver -VMHost $esx -ntpserver $ThisNTP -Confirm:$false
+    }
+    
+    Write-Host "Configuring NTP Client Policy on $esx" -ForegroundColor Green
+    Get-VMHostService -VMHost $esx | where{$_.Key -eq "ntpd"} | Set-VMHostService -Policy "on" -Confirm:$false
+
+    Write-Host "Restaring NTP Client  on $esx" -ForegroundColor Green
+    Get-VMHostService -VMHost $esx | where{$_.Key -eq "ntpd"} | Restart-VMHostService -Confirm:$false
+
+   
+    Write-Host "=========================================================================================" -ForegroundColor Cyan
+}
+
